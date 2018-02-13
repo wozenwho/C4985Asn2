2fcfc160aab85a64d21ce520d7fa582832d332dc
wozenwho <wilson.huu@gmail.com>
2018-02-12 21:04:11 -0800

    Test implementation of threaded UDP Recv

--- COMP4985_A2/Receive.cpp
+++ COMP4985_A2/Receive.cpp
@@ -1,5 +1,7 @@
 #include "Receive.h"
 
+
+
 int recvTCP(char* ipAddr)
 {
 	HANDLE sendThread;
@@ -17,18 +19,10 @@ int recvUDP(char* ipAddr)
 {
 	HANDLE sendThread;
 	DWORD threadID;
-	SOCKET AcceptSocket;
-	WSAEVENT AcceptEvent;
 	struct sockaddr_in sender;
 	recvThrdParam rtp;
 	int errorUDP;
 	char buffer[MAX_PACK_SIZE];
-
-	socketRecv = WSASocket(AF_INET, SOCK_DGRAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	if (socketRecv == INVALID_SOCKET)
-	{
-		errorUDP = WSAGetLastError();
-	}
 
 	memset((char*)&sender, 0, sizeof(sockaddr_in));
 	sender.sin_family = AF_INET;
@@ -36,12 +30,46 @@ int recvUDP(char* ipAddr)
 	sender.sin_addr.s_addr = inet_addr(ipAddr);
 	rtp.sender = sender;
 
+	socketRecv = WSASocket(AF_INET, SOCK_DGRAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (socketRecv == INVALID_SOCKET)
+	{
+		errorUDP = WSAGetLastError();
+	}
+
+	//Create thread to handle receiving packets from socket
+	//sendThread = CreateThread();
+
 	if (bind(socketRecv, (PSOCKADDR)&sender, sizeof(sockaddr_in)) == 0)
 	{
 		//ERROR Binding socket
 		errorUDP = WSAGetLastError();
 		exit(1);
 	}
+
+	return 0;
+}
+
+DWORD WINAPI recvTCPThread(LPVOID lpParam)
+{
+	return 0;
+}
+
+DWORD WINAPI recvUDPThread(LPVOID lpParam)
+{
+	char buffer[MAX_BUFFER_LENGTH];
+	recvThrdParam* rtp = (recvThrdParam*)lpParam;
+	sockaddr_in saFrom;
+	int error;
+	int errorUDP;
+	BOOL condition = true;
+
+	WSAEVENT AcceptEvent;
+	SOCKET AcceptSocket;
+	WSAEVENT EventArray[1];
+	LPSOCKET_INFORMATION SocketInfo;
+	DWORD Flags;
+	DWORD RecvBytes;
+
 
 	if (listen(socketRecv, 5))
 	{
@@ -56,23 +84,28 @@ int recvUDP(char* ipAddr)
 	}
 
 	AcceptSocket = accept(socketRecv, NULL, NULL);
+	if ((SocketInfo = (LPSOCKET_INFORMATION)GlobalAlloc(GPTR, sizeof(SOCKET_INFORMATION))) == NULL)
+	{
+		error = GetLastError();
+		exit(1);
+	}
 
+	SocketInfo->Socket = AcceptSocket;
+	ZeroMemory(&(SocketInfo->Overlapped), sizeof(WSAOVERLAPPED));
+	SocketInfo->BytesSEND = 0;
+	SocketInfo->BytesRECV = 0;
+	SocketInfo->DataBuffer.len = MAX_BUFFER_LENGTH;
+	SocketInfo->DataBuffer.buf = SocketInfo->Buffer;
 
-	return 0;
-}
-
-DWORD WINAPI recvTCPThread(LPVOID lpParam)
-{
-
-}
-
-DWORD WINAPI recvUDPThread(LPVOID lpParam)
-{
-	recvThrdParam* rtp = (recvThrdParam*)lpParam;
-	char buffer[MAX_BUFFER_LENGTH];
-	sockaddr_in saFrom;
-
-	WSARecvFrom(socketRecv, /*WSABUFptr*/, /**/, NULL, NULL, );
+	while (condition)
+	{
+		if (WSARecvFrom(SocketInfo->Socket, &(SocketInfo->DataBuffer), 1, &RecvBytes, &Flags, NULL, NULL,
+			&(SocketInfo->Overlapped), NULL) == SOCKET_ERROR)
+		{
+			errorUDP = WSAGetLastError();
+		}
+	}
+	
 
 }
 
