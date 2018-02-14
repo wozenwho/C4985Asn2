74dc94e8f30dc62d51f5a2228a911f1efe2d837a
werz <wilson.huu@gmail.com>
2018-02-13 18:36:31 -0800

    Incomplete implementation of senderTCP

--- COMP4985_A2/Send.cpp
+++ COMP4985_A2/Send.cpp
@@ -4,61 +4,48 @@
 
 int sendTCP(char* ipAddr, int packSize, int numPacks)
 {
+	struct sockaddr_in recvAddr;
+	struct sockaddr_in localAddr;
+	struct hostent *localHost;
+	char* ip;
 	HANDLE sendThread;
 	DWORD threadID;
-	struct sockaddr_in receiver;
-	sendThrdParam stp;
-	char defaultBuffer[MAX_PACK_SIZE];
-	int errorTCP;
+	sendThrdParam* stp = (sendThrdParam*)malloc(sizeof(sendThrdParam));
 
+	int error;
 
-	socketSend = socket(PF_INET, SOCK_STREAM, 0);
-	if (socketSend == INVALID_SOCKET)
-	{
-		errorTCP = WSAGetLastError();
-		MessageBox(hwnd, "socket init error in sendTCP", NULL, NULL);
-	}
+	recvAddr.sin_family = AF_INET;
+	recvAddr.sin_port = htons(PORT_NO);
+	recvAddr.sin_addr.s_addr = inet_addr(ipAddr);
 
-	memset((char*) &receiver, 0, sizeof(sockaddr_in));
-	receiver.sin_family = AF_INET;
-	receiver.sin_port = htons(PORT_NO);
-	receiver.sin_addr.s_addr = inet_addr(ipAddr);
+	localHost = gethostbyname("");
+	ip = inet_ntoa(*(struct in_addr *) *localHost->h_addr_list);
+	localAddr.sin_family = AF_INET;
+	localAddr.sin_port = htons(PORT_NO);
+	localAddr.sin_addr.s_addr = inet_addr(ip);
 
+	stp->receiver = recvAddr;
+	stp->sender = localAddr;
+	stp->packSize = packSize;
+	stp->numPacks = numPacks;
+	stp->filePtr = NULL;
 
-	sendThread = CreateThread(NULL, 0, sendTCPThread, NULL, 0, &threadID);
-
-
-	//CreateEvent();
+	sendThread = CreateThread(NULL, 0, sendTCPThread, (LPVOID)stp, 0, &threadID);
+	
 	return 0;
 }
 
 int sendUDP(char* ipAddr, int packSize, int numPacks)
 {
-	WSADATA wsaData;
-	WSABUF DataBuf;
-	WSAOVERLAPPED Overlapped;
 	struct sockaddr_in recvAddr;
 	struct sockaddr_in localAddr;
-	int RecvAddrSize = sizeof(recvAddr);
-	int LocalAddrSize = sizeof(localAddr);
-
 	struct hostent *localHost;
 	char *ip;
-
-	char *targetip;
-	char *targetport;
-
-	char SendBuf[1024] = "Data buffer to send";
-	int BufLen = 1024;
-	DWORD BytesSent = 0;
-	DWORD Flags = 0;
-
 	HANDLE sendThread;
 	DWORD threadID;
 	sendThrdParam* stp = (sendThrdParam*) malloc(sizeof(sendThrdParam));
 
 	int error;
-	int retval = 0;
 
 	recvAddr.sin_family = AF_INET;
 	recvAddr.sin_port = htons(PORT_NO);
@@ -87,16 +74,42 @@ int sendUDP(char* ipAddr, int packSize, int numPacks)
 
 DWORD WINAPI sendTCPThread(LPVOID lpParam)
 {
-	sendThrdParam* stp = (sendThrdParam*) lpParam;
-	char defaultBuffer[MAX_PACK_SIZE];
+	sendThrdParam* stp = (sendThrdParam*)lpParam;
+	WSADATA wsaData;
+	WSABUF wsaBuf;
 
-	for (int i = 0; i < stp->numPacks; i++)
+	WSAOVERLAPPED Overlapped;
+
+	struct sockaddr_in localAddr;
+	int localAddrSize = sizeof(localAddr);
+
+	char sendBuf[MAX_BUFFER_LENGTH];
+	DWORD BytesSent = 0;
+	DWORD Flags = 0;
+
+	int errorTCP;
+	int error;
+
+	ZeroMemory((PVOID)&Overlapped, sizeof(WSAOVERLAPPED));
+	Overlapped.hEvent = WSACreateEvent();
+	if (Overlapped.hEvent == WSA_INVALID_EVENT)
 	{
-		memset(defaultBuffer, (char)65, stp->packSize);
-		sendto(socketSend, defaultBuffer, stp->packSize, 0, (struct sockaddr*)&(stp->receiver), sizeof(stp->receiver));
+		errorTCP = WSAGetLastError();
+	}
+	
+	socketSend = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (socketSend == INVALID_SOCKET)
+	{
+		errorTCP = WSAGetLastError();
+		WSACloseEvent(Overlapped.hEvent);
+		WSACleanup();
 	}
 
-	//createevent?
+	if (connect(socketSend, (sockaddr*)&(stp->receiver), sizeof(stp->receiver)) == -1)
+	{
+		errorTCP = WSAGetLastError();
+	}
+
 	return 0;
 }
 
@@ -124,6 +137,7 @@ DWORD WINAPI sendUDPThread(LPVOID lpParam)
 	int errorUDP;
 	int error;
 	int retval = 0;
+	int numSent = 0;
 		
 	threadInfo  = *(sendThrdParam*)lpParam;
 
@@ -164,7 +178,23 @@ DWORD WINAPI sendUDPThread(LPVOID lpParam)
 		memset(DataBuf.buf, 'A', threadInfo.packSize);
 		for (int i = 0; i < threadInfo.numPacks; i++)
 		{
-			WSASendTo(socketSend, &DataBuf, 1, &BytesSent, Flags, (SOCKADDR *)&threadInfo.receiver, RecvAddrSize, &Overlapped, NULL);
+			wsaResult = WSASendTo(socketSend, &DataBuf, 1, &BytesSent, Flags, (SOCKADDR *)&threadInfo.receiver, RecvAddrSize, &Overlapped, NULL);
+			if ((wsaResult == SOCKET_ERROR) && (WSA_IO_PENDING != (error = WSAGetLastError()))) {
+				WSACloseEvent(Overlapped.hEvent);
+				closesocket(socketSend);
+				WSACleanup();
+				exit(1);
+			}
+			wsaResult = WSAWaitForMultipleEvents(1, &Overlapped.hEvent, TRUE, 1, TRUE);
+			if (wsaResult == WSA_WAIT_FAILED) {
+				errorUDP = WSAGetLastError();
+			}
+			wsaResult = WSAGetOverlappedResult(socketSend, &Overlapped, &BytesSent, FALSE, &Flags);
+			if (wsaResult == FALSE) {
+				errorUDP = WSAGetLastError();
+			}
+			else
+				numSent++;
 		}
 	}
 
@@ -180,5 +210,5 @@ DWORD WINAPI sendUDPThread(LPVOID lpParam)
 	//	sendto(socketSend, defaultBuffer, stp->packSize, 0, (struct sockaddr*)&(stp->receiver), sizeof(stp->receiver));
 	//}
 	////createevent?
-	return 0;
+	return numSent;
 }
